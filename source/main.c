c39d9ee423ae47e0dd4a3326718db012c251e1da
Luis <147719766+luisgs231@users.noreply.github.com>
2024-08-21 19:40:30 +0200

    Implemented hardware PWM

--- source/main.c
+++ source/main.c
@@ -1,30 +1,34 @@
 #include <12F683.h>
 #device ADC=10
 #fuses NOWDT, NOPROTECT, NOMCLR, INTRC, INTRC_IO
-#use delay(internal=8000000)
+#use delay(internal=4000000)
 #use standard_io(a)
 
 #define throttlePin          sAN3      // Pin 3
-#define brakePin             PIN_A2    // Pin 5
-#define signalPin            PIN_A0    // Pin 7
+#define brakePin             PIN_A0    // Pin 5
+#define signalPin            PIN_A2    // Pin 7
 
-#use pwm(CCP1,TIMER=2,FREQUENCY=200,DUTY=50)
+#use pwm(CCP1,TIMER=2,OUTPUT=signalPin,FREQUENCY=250)
 
+unsigned int16 duty = 373;
 void brake();
 
-#INT_EXT
-void EXT_isr(void)
+#INT_RA
+void RA_isr(void)
 {
-   output_toggle(signalPin);
-   brake();
+   if (!input(brakePin))
+   {
+      brake();
+   }
+   clear_interrupt(INT_RA);
 }
 
 void setup()
 {
-   delay_ms(10);
-   EXT_INT_EDGE(0, H_TO_L);
+   pwm_off();
+   //pwm_set_frequency(pulseFrequency);
    enable_interrupts(GLOBAL);
-   PORT_A_PULLUPS(0b000100);
+   PORT_A_PULLUPS(0b000001);
    setup_adc(ADC_CLOCK_DIV_32);
    setup_adc_ports(throttlePin, VSS_VDD);
    set_adc_channel(3);
@@ -33,7 +37,7 @@ void setup()
 void calibrate()
 {
    while(input(brakePin));    // wait for the brake signal
-   delay_ms(50);              // debounce delay
+   delay_ms(100);              // debounce delay
    while(!input(brakePin));   // wait for the brake release
    while(input(brakePin))     // wait for the brake signal
    {
@@ -44,20 +48,23 @@ void calibrate()
 
 void brake()
 {
-   // pulse neutral    // TODO PWM
+   pwm_set_duty(++duty);
    delay_ms(100);
    while(!input(brakePin));   // wait for the brake release
-   clear_interrupt(INT_EXT);
+   clear_interrupt(INT_RA);
    // re-sample before setting pwm!
 }
 
 void run()
 {
-   clear_interrupt(INT_EXT);
-   enable_interrupts(INT_EXT);
+   clear_interrupt(INT_RA);
+   enable_interrupts(INT_RA);
    // pulse neutral    // TODO PWM
+   pwm_set_duty(duty);
+   pwm_on();
    while(true)
    {
+      
       // poll throttle  // TODO ADC
       // map values     // TODO map()
       // set PWM        // TODO PWM
